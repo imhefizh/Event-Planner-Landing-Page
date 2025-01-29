ef071e0c9b2263a1e75e4fb5c572e5f9bec0b82f
imhefizh <hafidzsml@gmail.com>
2025-01-29 03:11:19 -0500

    complete

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>It is the website's footer that you can ignore</p>
+            <p>This website told me about how to use function component on React</p>
+        </footer>
+    )
+}
+
